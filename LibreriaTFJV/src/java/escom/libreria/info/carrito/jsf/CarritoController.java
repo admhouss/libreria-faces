b40338f695bcd2446a62a77bf4106142e8063327
aaron.jazhiel@hotmail.com <aaron.jazhiel@hotmail.com@a86b607a-05b7-e549-40c9-e2b7f718dd94>
2011-10-02 03:08:17 +0000

    mensaje de prueba

---
+++ LibreriaTFJV/src/java/escom/libreria/info/carrito/jsf/CarritoController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package escom.libreria.info.carrito.jsf;
+
+import escom.libreria.info.articulo.jpa.Articulo;
+import escom.libreria.info.articulo.jsf.util.JsfUtil;
+import escom.libreria.info.carrito.ejb.CarritoCompraTemporalLocal;
+import escom.libreria.info.login.sistema.SistemaController;
+import java.util.List;
+import javax.ejb.EJB;
+
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author xxx
+ */
+@ManagedBean(name="carritoController")
+@SessionScoped
+public class CarritoController {
+
+    @ManagedProperty("#{sistemaController}")
+    private SistemaController sistemaController;
+    @EJB private  escom.libreria.info.login.ejb.SistemaFacade sistemaFacade;
+
+
+    public SistemaController getSistemaController() {
+        return sistemaController;
+    }
+
+    public void setSistemaController(SistemaController sistemaController) {
+        this.sistemaController = sistemaController;
+    }
+
+    /** Creates a new instance of CarritoController */
+    public CarritoController() {
+        if(carritoCompraTemporalLocal==null)
+          carritoCompraTemporalLocal=sistemaFacade.getObtenerBandejaTemporal();
+    }
+ public void agregarArticulo(Articulo articulo){
+          carritoCompraTemporalLocal.addArticulo(articulo);
+         JsfUtil.addSuccessMessage("Articulo agregado Satisfactoriamente");
+    }
+    public void borrarArticulo(Articulo articulo){
+          carritoCompraTemporalLocal.removeArticulo(articulo);
+    }
+    public List<Articulo> getListArticulos(){
+        return carritoCompraTemporalLocal.getListArticulos();
+    }
+
+     private CarritoCompraTemporalLocal carritoCompraTemporalLocal;
+
+}
